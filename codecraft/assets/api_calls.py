3e43743a4c73efc8826e2c7528cda676646ef323
adya2004 <adyaneechadi@gmail.com>
2024-01-13 23:47:00 +0530

    api call functions are made to i) get languages\n ii) To compile the code\n iii) get status of compilled code

---
+++ codecraft/assets/api_calls.py
@@ -0,0 +1,81 @@
+import requests
+api_url = "https://ce.judge0.com/"
+
+def get_languages():
+    try:
+        url = api_url + "languages/"
+        response = requests.get(url)
+
+        response = response.json()
+        return(response)
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching languages: {e}")
+        return None
+
+
+def get_submission_token(source_code, id, inputs = None):
+    try:
+        url = "https://judge0-ce.p.rapidapi.com/submissions"
+
+        querystring = {"base64_encoded": "false", "fields": "*"}
+
+        payload = {
+            "language_id": id,
+            "source_code": source_code,
+            "stdin": inputs
+        }
+
+        headers = {
+            "content-type": "application/json",
+            "X-RapidAPI-Key": "48c41ea7afmsh5e5993471c69101p1cdae3jsnff7df8ad40a1",
+            "X-RapidAPI-Host": "judge0-ce.p.rapidapi.com"
+        }
+
+        response = requests.post(url, json=payload, headers=headers, params=querystring)
+
+        # print(response.json())
+        return response.json()['token']
+    except requests.exceptions.RequestException as e:
+        print(f"Error submitting code: {e}")
+        return None
+
+def check_submission_status(token):
+    try:
+        url = "https://judge0-ce.p.rapidapi.com/submissions/" + token
+
+        querystring = {"base64_encoded":"false","fields":"*"}
+
+        headers = {
+            "X-RapidAPI-Key": "48c41ea7afmsh5e5993471c69101p1cdae3jsnff7df8ad40a1",
+            "X-RapidAPI-Host": "judge0-ce.p.rapidapi.com"
+        }
+
+        response = requests.get(url, headers=headers, params=querystring)
+
+        response = response.json()
+        output = response['stdout']
+        error = response['stderr']
+        if output != None:
+            return output
+        else:
+            return error
+    except requests.exceptions.RequestException as e:
+        print(f"Error checking submission status: {e}")
+        return None
+
+
+def compile():
+    langs = get_languages()
+    for item in langs:
+        print("Langage:", item['name'], "  ---> id:", item['id'])
+    lang_id = int(input())
+    file_name = input()
+
+    with open(file_name, 'r', encoding='utf-8') as file:
+        source_code = file.read()
+
+    token = get_submission_token(source_code, lang_id)
+    ans = check_submission_status(token)
+    print("Output:\n", ans)
+
+# compile()
