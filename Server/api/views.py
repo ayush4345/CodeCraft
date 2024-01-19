498caf6655f76c78ac6e3f6a7acf4dfbb9c2ed56
adya2004 <adyaneechadi@gmail.com>
2024-01-19 13:59:07 +0530

    changed id to language_id

--- Server/api/views.py
+++ Server/api/views.py
@@ -12,4 +12,4 @@ class Languages(APIView):
     
 class Compile(APIView):
     def post(self, request):
-        return Response(compile(request.data["source_code"], request.data["id"]))
+        return Response(compile(request.data["source_code"], request.data["language_id"]))
